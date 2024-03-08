f37f31961113feb1780a5466a1fa386e2947a281
Noct2000 <oleg.kvasha.00@gmail.com>
2024-03-08 13:34:55 +0200

    [business-api]: fix 'GET: /payments/to-transaction' endpoint

--- business-api/src/main/java/com/example/business/api/service/impl/PaymentServiceImpl.java
+++ business-api/src/main/java/com/example/business/api/service/impl/PaymentServiceImpl.java
@@ -1,9 +1,12 @@
 package com.example.business.api.service.impl;
 
+import com.example.business.api.dto.response.BankTransactionResponseDto;
 import com.example.business.api.dto.response.PaymentResponseDto;
 import com.example.business.api.service.BankTransactionFeignClientService;
 import com.example.business.api.service.PaymentFeignClientService;
 import com.example.business.api.service.PaymentService;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -16,11 +19,19 @@ public class PaymentServiceImpl implements PaymentService {
 
     @Override
     public List<PaymentResponseDto> getPaymentsToTransaction() {
-        return bankTransactionFeignClientService.getLastWithUniquePaymentId().stream()
-                .map(
-                        bankTransaction -> paymentFeignClientService
-                                    .getById(bankTransaction.getPaymentId())
-                )
-                .toList();
+        List<BankTransactionResponseDto> lastTransactionsWithUniquePaymentId =
+                bankTransactionFeignClientService.getLastWithUniquePaymentId();
+        List<PaymentResponseDto> payments = new ArrayList<>();
+        for (BankTransactionResponseDto transaction: lastTransactionsWithUniquePaymentId) {
+            PaymentResponseDto payment = paymentFeignClientService
+                    .getById(transaction.getPaymentId());
+            LocalDateTime debitDateTime = transaction.getCreationTime()
+                    .plusMinutes(payment.getMinsBeforeDebiting());
+            LocalDateTime now = LocalDateTime.now();
+            if (debitDateTime.isBefore(now) || debitDateTime.isEqual(now)) {
+                payments.add(payment);
+            }
+        }
+        return payments;
     }
 }
