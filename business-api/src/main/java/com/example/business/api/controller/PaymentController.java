a1a79d30d76c01aa225d51831efe097f6ff8dffc
Noct2000 <oleg.kvasha.00@gmail.com>
2024-03-08 02:59:28 +0200

    [business-api]: add 'GET: /payments/by-inn/{inn}'

---
+++ business-api/src/main/java/com/example/business/api/controller/PaymentController.java
@@ -0,0 +1,22 @@
+package com.example.business.api.controller;
+
+import com.example.business.api.dto.response.PaymentResponseDto;
+import com.example.business.api.service.PaymentFeignClientService;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/payments")
+@RequiredArgsConstructor
+public class PaymentController {
+    private final PaymentFeignClientService paymentFeignClientService;
+
+    @GetMapping("/by-inn/{inn}")
+    List<PaymentResponseDto> getByPayment(@PathVariable String inn) {
+        return paymentFeignClientService.getByPayment(inn);
+    }
+}
---
+++ business-api/src/main/java/com/example/business/api/service/PaymentFeignClientService.java
@@ -0,0 +1,13 @@
+package com.example.business.api.service;
+
+import com.example.business.api.dto.response.PaymentResponseDto;
+import java.util.List;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@FeignClient(name = "payment", url = "${data-api}")
+public interface PaymentFeignClientService {
+    @GetMapping("/payments/by-inn/{inn}")
+    List<PaymentResponseDto> getByPayment(@PathVariable String inn);
+}
